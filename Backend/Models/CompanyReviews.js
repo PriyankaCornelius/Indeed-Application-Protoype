f27139c8ef5fc0f9c6a2ddcbe52d3e612bdb8079
Archita Chakraborty <archita22ind@gmail.com>
2021-11-11 18:46:06 -0800

    Adding Review collection model and kafka boilerplate code

---
+++ Backend/Models/CompanyReviews.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+var CompanyReviewsSchema = new Schema(
+  {
+    applicantId: { type: String, required: true },
+    companyId: { type: String, required: true },
+    reviewTitle: { type: String, required: true },
+    jobTitle: { type: String, required: true },
+    location: { type: String, required: true },
+    review: { type: String, required: true },
+    rating: { type: Number, required: false },
+    isFeatured: { type: Boolean, required: false },
+    isHelpful: { type: Number, required: false },
+    isNotHelpful: { type: Number, required: false },
+    createdAt: { type: Date, required: true },
+    modifiedAt: { type: Date, required: false },
+  },
+  {
+    versionKey: false,
+  }
+);
+
+const CompanyReviews = mongoose.model("CompanyReview", CompanyReviewsSchema);
+module.exports = CompanyReviews;
--- Middleware/README.md
+++
@@ -1,2 +0,0 @@
-# CMPE-273--Indeed-Application-Prototype
-Group Project CMPE 273- Indeed Application Prototype
