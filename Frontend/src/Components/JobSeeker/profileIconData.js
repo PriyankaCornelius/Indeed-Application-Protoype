e2e53c41109b7f71bf718419b90aa240862d99d9
Anay Dilip Naik <89814957+anaydilipnaik@users.noreply.github.com>
2021-11-26 18:54:16 -0800

    common logout end to end done

--- Frontend/src/Components/JobSeeker/profileIconData.js
+++ Frontend/src/Components/JobSeeker/profileIconData.js
@@ -6,8 +6,15 @@ import LogoutIcon from "@mui/icons-material/Logout";
 import { Menu, MenuItem, Typography } from "@mui/material";
 import { ListItemIcon } from "@material-ui/core";
 import { Link } from "react-router-dom";
+import { commonLogoutFunc } from "../../redux/actions/loginActions";
+import { connect } from "react-redux";
 
 const ProfileIconData = (props) => {
+  const handleLogout = (e) => {
+    e.preventDefault();
+    props.commonLogoutFunc();
+  };
+
   return (
     <React.Fragment>
       <Menu
@@ -72,15 +79,18 @@ const ProfileIconData = (props) => {
             My reviews
           </MenuItem>
         </Link>
-        <MenuItem>
-          <ListItemIcon>
-            <LogoutIcon fontSize="small" />
-          </ListItemIcon>
-          Sign out
-        </MenuItem>
+
+        <Link to="#" onClick={handleLogout} style={{ textDecoration: "none" }}>
+          <MenuItem>
+            <ListItemIcon>
+              <LogoutIcon fontSize="small" />
+            </ListItemIcon>
+            Sign out
+          </MenuItem>
+        </Link>
       </Menu>
     </React.Fragment>
   );
 };
 
-export default ProfileIconData;
+export default connect(null, { commonLogoutFunc })(ProfileIconData);
